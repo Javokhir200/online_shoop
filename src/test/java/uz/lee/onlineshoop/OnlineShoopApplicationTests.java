e84edcf5e85fff0aad628e79485247af727f6186
javokhir200 <javokhirganiboyev2004@gmail.com>
2024-06-22 16:04:54 +0500

    first launch

---
+++ src/test/java/uz/lee/onlineshoop/OnlineShoopApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.lee.onlineshoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
