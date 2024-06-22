ad63bd917f93ec0616c19e9079796d7e7c6108bf
stranger <ismoiljonovabdulaziz97@gmail.com>
2024-06-22 18:00:15 +0500

    Entities created check first

---
+++ src/main/java/uz/lee/onlineshoop/entity/ProductEntity.java
@@ -0,0 +1,29 @@
+package uz.lee.onlineshoop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private Type type;
+    private String price;
+    private Integer count;
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private StoreEntity store;
+    private String description;
+}
