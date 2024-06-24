0041723d54fc8d859c38490ae3ca40bb1c2d3663
ASUS <a88840934@gmail.com>
2024-06-24 14:39:02 +0500

    OrderController is created

---
+++ src/main/java/uz/lee/onlineshoop/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package uz.lee.onlineshoop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.lee.onlineshoop.entity.OrderEntity;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
+    boolean existsById(Long id);
+}
